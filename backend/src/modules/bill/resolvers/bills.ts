d3dd205fe25e787de888bb044fe4abcbcba50d3b
Luis Guerrero <luis.guerrero@capgemini.com>
2022-01-06 13:59:20 -0600

    remove installments when querying list of bills

--- backend/src/modules/bill/resolvers/bills.ts
+++ backend/src/modules/bill/resolvers/bills.ts
@@ -1,5 +1,4 @@
 import Bill from '../../../models/bill';
-import Installment from '../../../models/installment';
 import { IUser } from '../../../models/user';
 
 type Context = {
@@ -10,12 +9,7 @@ const bills = async (_parent: any, _args: any, context: Context) => {
   const { user } = context;
   const dbBills = await Bill.find({ user: user.id });
 
-  return dbBills.map(async (bill) => ({
-    installments: await Installment.find({ bill: bill.id }),
-    installmentsNumber: bill.installmentsNumber,
-    name: bill.name,
-    total: bill.total,
-  }));
+  return dbBills;
 };
 
 export default bills;
